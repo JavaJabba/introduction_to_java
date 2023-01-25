dcfeda1df50c342ba5d7ee5cf66527cc1871352a
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2023-01-25 14:28:38 +0000

    intro

---
+++ Box.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+class Box{
+	public static void main (String[] args){
+		JFrame myWindow;
+		myWindow=new JFrame();
+		myWindow.setSize (300,200);
+		myWindow.setTitle("Java prog for box creation");
+		myWindow.setVisible(true);
+	}
+}
