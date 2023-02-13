62dcf040c2c9b58703507a82789a5ecf41f6c488
JavaJabba <dylanmurray1997@gmail.com>
2023-02-13 21:33:18 +0000

    well

---
+++ Labs/loan.java
@@ -0,0 +1,25 @@
+package Labs;
+
+import java.lang.reflect.Array;
+import java.util.Dictionary;
+import java.util.List;
+
+import javax.lang.model.type.ArrayType;
+
+public class loan {
+    int loanAmount, income;
+    int[] empIncomes = {1500, 700, 100};
+
+    public loan(){
+
+        for (int income : empIncomes){
+            switch(income){
+                case 1000:
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
